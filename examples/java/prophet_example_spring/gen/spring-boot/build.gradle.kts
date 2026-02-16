f318b7ccd79c286160e40635c1a1f44f503ae0c4
Chainso <chanzobryan@gmail.com>
2026-02-15 23:45:46 -0800

    Refresh Java snapshot outputs for runtime 0.2.5

--- examples/java/prophet_example_spring/gen/spring-boot/build.gradle.kts
+++ examples/java/prophet_example_spring/gen/spring-boot/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("io.github.chainso:prophet-events-runtime:0.2.4")
+    implementation("io.github.chainso:prophet-events-runtime:0.2.5")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
